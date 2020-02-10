19eff9e687285809e60a6e068883120a81b6e99c
1321200 <55232721+A2JA2@users.noreply.github.com>
2020-02-10 10:57:02 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordArray
+{
+    class Program
+    {
+
+        struct Student
+        {
+            public int StudentID; public string Surname; public string Forename; public int Age;
+            public bool Enrolled;
+        };
+
+        static void Main(string[] args)
+        {
+            // create the record structure struct student
+         
+        Student mystudent; // declare the 'mystudent' student structure type
+
+        //initialise the elements in the record structure
+        mystudent.StudentID = 2001;
+            mystudent.Surname = "Brush";
+            mystudent.Forename = "Basil";
+            mystudent.Age = 21;
+            mystudent.Enrolled = true;
+
+            //print the data in the record structure
+            Console.WriteLine("ID        : {0}", mystudent.StudentID);
+            Console.WriteLine("Surname   : {0}", mystudent.Surname);
+            Console.WriteLine("Firstname : {0}", mystudent.Forename);
+            Console.WriteLine("Age       : {0}", mystudent.Age);
+            Console.WriteLine("Enrolled  : {0}", mystudent.Enrolled);
+            //wait for key press to continue
+            Console.ReadKey();
+        }
+    }
+}
